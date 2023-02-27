3beddee8449ee2a8514a0f8933327bec4c5892d2
mfarrasi <farras.ilhami@mail.ugm.ac.id>
2023-02-27 10:07:54 +0700

    update: index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Muhammad Farras Ilhami] | [456843]
+            Muhammad Farras Ilhami | 456843
           </h5>
         </div>
       </main>
